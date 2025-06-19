5c987339706aa1ad3505d7f6e31fc7c3d96eea6b
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2025-06-19 18:21:17 -0500

    Update RPC and wallet tools for Python 3

--- contrib/wallettools/walletunlock.py
+++ contrib/wallettools/walletunlock.py
@@ -1,4 +1,5 @@
+#!/usr/bin/env python3
 from jsonrpc import ServiceProxy
 access = ServiceProxy("http://127.0.0.1:8332")
-pwd = raw_input("Enter wallet passphrase: ")
+pwd = input("Enter wallet passphrase: ")
 access.walletpassphrase(pwd, 60)
