c2cef204024c1249833c78500c03763ff673b334
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2025-06-19 17:55:00 -0500

    Update scripts for Python3

--- share/qt/make_spinner.py
+++ share/qt/make_spinner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # W.J. van der Laan, 2011
 # Make spinning .mng animation from a .png
 # Requires imagemagick 6.7+
@@ -26,7 +26,7 @@ def frame_to_filename(frame):
     return path.join(TMPDIR, TMPNAME % frame)
 
 frame_files = []
-for frame in xrange(NUMFRAMES):
+for frame in range(NUMFRAMES):
     rotation = (frame + 0.5) / NUMFRAMES * 360.0
     if CLOCKWISE:
         rotation = -rotation
