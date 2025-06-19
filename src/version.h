4109314883cdc3dfcbc14dbc921c479f9d4707b9
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2025-06-19 17:01:04 -0500

    Correct spelling in version header comment

--- src/version.h
+++ src/version.h
@@ -32,7 +32,7 @@ static const int DATABASE_VERSION = 70509;
 
 static const int PROTOCOL_VERSION = 60014;
 
-// intial proto version, to be increased after version/verack negotiation
+// initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
